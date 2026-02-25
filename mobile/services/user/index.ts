065f659b79386d7cd9180907a5308b5f4c933f3e
TruongNguyenThaiBinh77 <binhtntse182370@fpt.edu.vn>
2026-02-25 13:16:24 +0700

    iot

--- mobile/services/user/index.ts
+++ mobile/services/user/index.ts
@@ -1,6 +1,7 @@
 // User Services - Export all user-related API services
 export { default as authService } from './authService';
 export { default as lockerService } from './lockerService';
+export { default as loyaltyService } from './loyaltyService';
 export { default as notificationService } from './notificationService';
 export { default as orderService } from './orderService';
 export { default as paymentService } from './paymentService';
@@ -11,6 +12,7 @@ export { default as userService } from './userService';
 // Re-export individual functions for convenience
 export * from './authService';
 export * from './lockerService';
+export * from './loyaltyService';
 export * from './notificationService';
 export * from './orderService';
 export * from './paymentService';
